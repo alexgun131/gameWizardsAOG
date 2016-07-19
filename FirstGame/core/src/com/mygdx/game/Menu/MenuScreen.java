1ba6984692893cd1f433b350ba657ab0a48a12c2
lastre <orionbl@hotmail.es>
2016-07-19 17:07:35 +0200

    [Menu] Add buttons

--- FirstGame/core/src/com/mygdx/game/Menu/MenuScreen.java
+++ FirstGame/core/src/com/mygdx/game/Menu/MenuScreen.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Align;
@@ -38,10 +39,48 @@ public class MenuScreen extends InputAdapter implements Screen {
     int select = -1;
     Music music;
 
+    Texture FlyButton;
+    Texture WormButton;
+    Texture FishButton;
+    TextureRegion[] FlyButtonSprite;
+    TextureRegion[] WormButtonSprite;
+    TextureRegion[] FishButtonSprite;
+    float flyFps;
+    float wormFps;
+    float fishFps;
+
     public MenuScreen(FirstGame game, Music music){
         this.game = game;
         this.music = music;
+        loadTextures();
+        flyFps = 0.0f;
+        wormFps = 0.0f;
+        fishFps = 0.0f;
     }
+
+    private void loadTextures(){
+        // TEXTURES
+        // Background
+        int buttonSize = 256;
+        int animationColumns = 2;
+        int animationRows = 1;
+
+        FlyButton = new Texture("FlyButton.png");
+        WormButton = new Texture("WormButton.png");
+        FishButton = new Texture("FishButton.png");
+
+        FlyButtonSprite = new TextureRegion[animationColumns*animationRows];
+        WormButtonSprite = new TextureRegion[animationColumns*animationRows];
+        FishButtonSprite = new TextureRegion[animationColumns*animationRows];
+
+        for (int i = 0; i < animationColumns; i++) {
+            FlyButtonSprite[i] = new TextureRegion(FlyButton, buttonSize*i, 0, buttonSize, buttonSize);
+            WormButtonSprite[i] = new TextureRegion(WormButton, buttonSize*i, 0, buttonSize, buttonSize);
+            FishButtonSprite[i] = new TextureRegion(FishButton, buttonSize*i, 0, buttonSize, buttonSize);
+        }
+
+    }
+
     @Override
     public void show() {
 
@@ -98,6 +137,11 @@ public class MenuScreen extends InputAdapter implements Screen {
 
         batch.begin();
 
+        batch.draw(FlyButtonSprite[getFlySprite(delta)], CONSTANTS.MENU_OPTIONS.x-CONSTANTS.MENU_BUBBLE_RADIUS*2, CONSTANTS.MENU_OPTIONS.y-CONSTANTS.MENU_BUBBLE_RADIUS*2, CONSTANTS.MENU_BUBBLE_RADIUS*4, CONSTANTS.MENU_BUBBLE_RADIUS*4);
+        batch.draw(WormButtonSprite[getWormSprite(delta)], CONSTANTS.MENU_PLAYGAME.x-CONSTANTS.MENU_BUBBLE_RADIUS*2, CONSTANTS.MENU_PLAYGAME.y-CONSTANTS.MENU_BUBBLE_RADIUS*2, CONSTANTS.MENU_BUBBLE_RADIUS*4, CONSTANTS.MENU_BUBBLE_RADIUS*4);
+        batch.draw(FishButtonSprite[getFishSprite(delta)], CONSTANTS.MENU_SCORES.x-CONSTANTS.MENU_BUBBLE_RADIUS*2, CONSTANTS.MENU_SCORES.y-CONSTANTS.MENU_BUBBLE_RADIUS*2, CONSTANTS.MENU_BUBBLE_RADIUS*4, CONSTANTS.MENU_BUBBLE_RADIUS*4);
+
+
         final GlyphLayout easyLayout = new GlyphLayout(font, CONSTANTS.OPTIONS_LABEL[languaje]);
         font.draw(batch, CONSTANTS.OPTIONS_LABEL[languaje], CONSTANTS.MENU_OPTIONS.x, CONSTANTS.MENU_OPTIONS.y + easyLayout.height / 2, 0, Align.center, false);
 
@@ -109,6 +153,36 @@ public class MenuScreen extends InputAdapter implements Screen {
 
         batch.end();
 
+    }
+
+    private int getFlySprite(float delta) {
+        flyFps += delta;
+        flyFps %= 100;
+        int sprite = 0;
+        if ((flyFps%0.3)>0.15){
+            sprite = 1;
+        }
+        return sprite;
+    }
+
+    private int getWormSprite(float delta) {
+        wormFps += delta;
+        wormFps %= 100;
+        int sprite = 0;
+        if ((wormFps%0.6)>0.3){
+            sprite = 1;
+        }
+        return sprite;
+    }
+
+    private int getFishSprite(float delta) {
+        fishFps += delta;
+        wormFps %= 100;
+        int sprite = 0;
+        if ((wormFps%0.5)>0.25){
+            sprite = 1;
+        }
+        return sprite;
     }
 
     @Override
