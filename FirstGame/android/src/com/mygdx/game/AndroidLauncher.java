a4d4755535584dfc08715d46db5438ff9207d9e1
alexgun131 <alex.gonz.gonzalez@gmail.com>
2016-07-23 21:14:29 +0200

    addview ID beta for debug

--- FirstGame/android/src/com/mygdx/game/AndroidLauncher.java
+++ FirstGame/android/src/com/mygdx/game/AndroidLauncher.java
@@ -30,7 +30,7 @@ public class AndroidLauncher extends AndroidApplication {
 
 		adView = new AdView(this);
 		adView.setAdUnitId(getString(R.string.banner_ad_unit_id)); // ca-app-pub-9401292122550373/3606029643 esta mal, falta el 3 para que no nos baneen
-		adView.setAdSize(AdSize.BANNER);
+		adView.setAdSize(AdSize.BANNER); // el de pau era ca-app-pub-3940256099942544/6300978111
 		adView.loadAd(new AdRequest.Builder().build());
 
 		RelativeLayout.LayoutParams adParams =
